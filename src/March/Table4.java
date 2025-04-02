cf5725c05cf407d27a3416e70a3cf740868f58b1
sreenukunala <sreenu4,kunala@gmail.com>
2025-04-02 10:20:49 +0530

    add all files

---
+++ src/March/Table4.java
@@ -0,0 +1,30 @@
+package March;
+
+public class Table4
+{
+    public static void main(String[] args)
+    {
+       int table = 4;
+       System.out.printf(" % d x  %d = %d" ,table, 1, table*1);
+       System.out.println();
+       System.out.printf(" % d x  %d = %d" ,table, 2, table*2);
+       System.out.println();
+       System.out.printf(" % d x  %d = %d" ,table, 3, table*3);
+       System.out.println();
+       System.out.printf(" % d x  %d = %d" ,table, 4, table*4);
+       System.out.println();
+       System.out.printf(" % d x  %d = %d" ,table, 5, table*5);
+        System.out.println();
+        System.out.printf(" % d x  %d = %d" ,table, 6, table*6);
+        System.out.println();
+        System.out.printf(" % d x  %d = %d" ,table, 7, table*7);
+        System.out.println();
+        System.out.printf(" % d x  %d = %d" ,table, 8, table*8);
+        System.out.println();
+        System.out.printf(" % d x  %d = %d" ,table, 9, table*9);
+        System.out.println();
+        System.out.printf(" % d x  %d = %d" ,table, 10, table*10);
+
+
+    }
+}
